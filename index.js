ab80aeab0b9a992b2645192e65049e7c3b5bed2d
Asad114-al <as3ad.94aa@gmail.com>
2020-12-14 15:38:18 +0100

    404 page

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 
 const express = require('express')
 const app = express()
-
 console.log(__dirname);
-
-
-
 app.use(express.static('public'));
-
 
 app.get('/', (request, response) => {
     response.sendFile('./views/home.html', { root: __dirname })
 })
-
 app.get('/Community', (req, res) => {
    
     res.sendFile('./views/Community.html', { root: __dirname })
@@ -26,9 +20,7 @@ app.get('/about', (req, res) => {
 app.get('/pricing', (req,res) => {
     res.sendFile('./views/pricing.html', {root: __dirname})
 })
-app.get('/about', (denise, kim) => {
-    kim.redirect('/about')
-})
+
 
 
 app.use((req, res) => {
