b9d172caf1a53e5174b0ce81614394b7533bccb7
Asad114-al <as3ad.94aa@gmail.com>
2020-12-14 14:47:38 +0100

    first commint

---
+++ index.js
@@ -0,0 +1,38 @@
+
+const express = require('express')
+const app = express()
+
+console.log(__dirname);
+
+
+
+app.use(express.static('public'));
+
+
+app.get('/', (request, response) => {
+    response.sendFile('./views/home.html', { root: __dirname })
+})
+
+app.get('/Community', (req, res) => {
+   
+    res.sendFile('./views/Community.html', { root: __dirname })
+})
+app.get('/Solutions', (req, res) => {
+    res.sendFile('./views/Solutions.html', { root: __dirname })
+})
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname })
+})
+app.get('/pricing', (req,res) => {
+    res.sendFile('./views/pricing.html', {root: __dirname})
+})
+app.get('/about', (denise, kim) => {
+    kim.redirect('/about')
+})
+
+
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', { root: __dirname })
+})
+
+app.listen(5000, () => console.log('Server running ...'))
